4e026b6d5e9edb919a8b812af978c9ea4b17eb99
Asif <asif.raza@fissionlabs.com>
2023-02-06 12:43:00 +0530

    written user class

--- user-management-coe/src/main/java/com/fissionlabs/coe/UserManagementCoeApplication.java
+++ user-management-coe/src/main/java/com/fissionlabs/coe/UserManagementCoeApplication.java
@@ -3,11 +3,17 @@ package com.fissionlabs.coe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fissionlabs.coe.domain.User;
+
 @SpringBootApplication
 public class UserManagementCoeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementCoeApplication.class, args);
+		
 	}
+	
+
+	
 
 }
---
+++ user-management-coe/src/main/java/com/fissionlabs/coe/domain/User.java
@@ -0,0 +1,63 @@
+package com.fissionlabs.coe.domain;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "username" }) })
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1684205632916084046L;
+	
+	@Id
+	@GeneratedValue(generator = "uuid")
+	private String id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String userRole;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+}
