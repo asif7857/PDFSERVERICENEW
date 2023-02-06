583b2e3da7b01e9b5337f57bb1cd3d026c318659
Asif <asif.raza@fissionlabs.com>
2023-02-06 12:21:26 +0530

    inital project folder uploaded

---
+++ user-management-coe/src/main/java/com/fissionlabs/coe/UserManagementCoeApplication.java
@@ -0,0 +1,13 @@
+package com.fissionlabs.coe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementCoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementCoeApplication.class, args);
+	}
+
+}
