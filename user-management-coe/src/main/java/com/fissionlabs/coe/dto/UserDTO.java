9e97b649ce464ef59df89209f90abd4749e91437
Asif <asif.raza@fissionlabs.com>
2023-02-06 12:55:40 +0530

    added repository and dto class

---
+++ user-management-coe/src/main/java/com/fissionlabs/coe/dto/UserDTO.java
@@ -0,0 +1,69 @@
+package com.fissionlabs.coe.dto;
+
+public class UserDTO {
+
+	private String id;
+    private String username;
+    private String name;
+    private String userRole;
+    private String status;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String workNumber;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getWorkNumber() {
+		return workNumber;
+	}
+	public void setWorkNumber(String workNumber) {
+		this.workNumber = workNumber;
+	}
+    
+}
